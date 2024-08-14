be4b8de07f94f7a6d92674330555451cada59ed3
Raegan Faith Paguirigan <57030395+raeganfaith@users.noreply.github.com>
2024-08-14 16:24:10 +0800

    Stop tracking backend/dist

--- backend/dist/utils/token-manager.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-export const createToken = (id, email, expiresIn) => {
-    const payload = { id, email };
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {
-        expiresIn,
-    });
-    return token;
-};
-//# sourceMappingURL=token-manager.js.map
