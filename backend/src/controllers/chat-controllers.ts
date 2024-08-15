bce016d55f7de74b6a1d1ea044209e12fe59fe1a
Raegan Faith Paguirigan <57030395+raeganfaith@users.noreply.github.com>
2024-08-15 18:13:23 +0800

    Added verifyToken middleware to /new chat route

---
+++ backend/src/controllers/chat-controllers.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Response, Request } from "express";
+
+export const generateChatCompletion = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  
+};
--- backend/src/routes/chat-routes.ts
+++ backend/src/routes/chat-routes.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { verifyToken } from "../utils/token-manager.js";
 
+// Protected API
 const chatRoutes = Router();
+chatRoutes.post("/new", verifyToken);
 
 export default chatRoutes;
