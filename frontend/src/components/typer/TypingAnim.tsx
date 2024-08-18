d956d57d4ed40b3991c6510990f3b5dce301c395
Raegan Faith Paguirigan <57030395+raeganfaith@users.noreply.github.com>
2024-08-18 18:45:37 +0800

    Updated color scheme and commented out Footer component in various pages

--- frontend/src/components/typer/TypingAnim.tsx
+++ frontend/src/components/typer/TypingAnim.tsx
@@ -15,7 +15,7 @@ const TypingAnim = () => {
       speed={50}
       style={{
         fontSize: "60px",
-        color: "white",
+        color: "#DBD8E3",
         display: "inline-block",
         textShadow: "1px 1px 20px #000",
       }}
