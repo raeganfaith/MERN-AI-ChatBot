9c21de4576b495474c19862f576b364bd4e0b410
Raegan Faith Paguirigan <57030395+raeganfaith@users.noreply.github.com>
2024-08-18 18:25:04 +0800

    Updated frontend code: added react-type-animation and @types/react-syntax-highlighter dependencies, modified App.tsx and App.css, updated index.css, and changed Signup.tsx and NotFound.tsx components.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,17 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import { Toaster } from "react-hot-toast";
 import axios from "axios";
 axios.defaults.baseURL = "http://localhost:5000/api/v1";
 axios.defaults.withCredentials = true;
-
 const theme = createTheme({
   typography: {
-    fontFamily: "Roboto Slab, serif",
+    fontFamily: "Roboto Slab,serif",
     allVariants: { color: "white" },
   },
 });
